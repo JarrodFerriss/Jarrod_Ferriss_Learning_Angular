0ad9cc5ab55567744f7353fbd6f5cbfdc377f6cc
Jarrod Ferriss <w0856197@myscc.ca>
2024-11-13 13:19:18 -0500

    implemented lazy loading

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,6 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter, Routes } from '@angular/router';
 import { AppComponent } from './app/app.component';
 import { SpaceMarineListComponent } from './app/space-marine-list/space-marine-list.component';
-import { SpaceMarineListItemComponent } from './app/space-marine-list-item/space-marine-list-item.component';
-import { PageNotFoundComponent } from './app/page-not-found/page-not-found.component';
-import { ModifyListItemComponent } from './app/modify-list-item/modify-list-item.component';
 
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { importProvidersFrom } from '@angular/core';
@@ -12,13 +9,33 @@ import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService } from './app/services/in-memory-data.service';
 import { FullNameWithRankPipe } from "./app/customPipe/full-name-with-rank.pipe";
 
-// Define the routes
 const routes: Routes = [
+  // Eager load the default route
   { path: '', redirectTo: '/space-marines', pathMatch: 'full' },
   { path: 'space-marines', component: SpaceMarineListComponent },
-  { path: 'space-marines/:id', component: SpaceMarineListItemComponent },
-  { path: 'modify-list-item', component: ModifyListItemComponent },
-  { path: '**', component: PageNotFoundComponent }
+
+  // Lazy load these routes
+  {
+    path: 'space-marines/:id',
+    loadComponent: () =>
+      import('./app/space-marine-list-item/space-marine-list-item.component').then(
+        m => m.SpaceMarineListItemComponent
+      )
+  },
+  {
+    path: 'modify-list-item',
+    loadComponent: () =>
+      import('./app/modify-list-item/modify-list-item.component').then(
+        m => m.ModifyListItemComponent
+      )
+  },
+  {
+    path: '**',
+    loadComponent: () =>
+      import('./app/page-not-found/page-not-found.component').then(
+        m => m.PageNotFoundComponent
+      )
+  }
 ];
 
 // Set up routing and bootstrap the application with HTTP and in-memory web API
